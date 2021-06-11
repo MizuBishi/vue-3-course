1ecbfe074de9a4dcde6fdbe0ab7d860d06fccd5d
Michelle Andrey <>
2021-06-11 12:10:53 +0200

    feat: finish assessment

---
+++ 08_componentCommunication/assignment/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <active-user :username="username" :age="age" />
+    <user-data @show-data="showData"></user-data>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "Michelle Andrey",
+      age: 35,
+    };
+  },
+  methods: {
+    showData(username, age) {
+      this.username = username;
+      this.age = age;
+      console.log("show data", username, age);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ 08_componentCommunication/assignment/src/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form @submit.prevent="showData">
+      <div>
+        <label>Username</label>
+        <input type="text" v-model="enteredUsername" />
+      </div>
+      <div>
+        <label>Age</label>
+        <input type="number" v-model="enteredAge" />
+      </div>
+      <button>Set User Data</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["show-data"],
+  data() {
+    return {
+      enteredUsername: "",
+      enteredAge: "",
+    };
+  },
+  methods: {
+    showData() {
+      this.$emit("show-data", this.enteredUsername, this.enteredAge);
+    },
+  },
+};
+</script>
+
+<style></style>
